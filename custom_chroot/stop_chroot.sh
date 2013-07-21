0c06a5091eb1e87a57a3cd5d836cbcad8f214469
bitardo <bitard.michael@gmail.com>
2013-07-21 18:03:56 +0200

    when stopping chroot, kill all processes associated to the chroot and unmount all

--- custom_chroot/stop_chroot.sh
+++ custom_chroot/stop_chroot.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 # -*- coding: UTF8 -*-
+pushd `dirname $0` > /dev/null
+scriptPath=`pwd`
+popd > /dev/null
+root_dir=$scriptPath/chroot
 echo "stop all things launched by ansible and stop the chroot"
+
+for rootProc in /proc/*/root; do
+        LINK=$(sudo readlink $rootProc)
+        if [ "x$LINK" != "x" ]; then
+                if [ "x${LINK:0:${#root_dir}}" = "x$root_dir" ]; then
+                        # this process is in the chroot...
+                        PID=$(basename $(dirname "$ROOT"))
+                        echo "$PID is in the chroot and is associated to `ps -p $PID -o comm=`, killing..."
+                        sudo kill -9 "$PID"
+                fi
+        fi
+done
+
+unmountFileSystems() {
+        sudo echo -ne "unmounting pseudo filesystems:"
+        for pseudo in dev dev/pts proc sys
+        do
+                sudo umount $root_dir/$pseudo
+                echo -ne " $pseudo"
+        done
+        echo ""
+}
+unmountFileSystems
