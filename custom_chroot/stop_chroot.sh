165dec3bc98fd59d7acf43ae326b42d3e0099bcc
bitardo <bitard.michael@gmail.com>
2013-07-21 22:29:33 +0200

    no more recuriv kill, unmounting in the good order

--- custom_chroot/stop_chroot.sh
+++ custom_chroot/stop_chroot.sh
@@ -8,7 +8,6 @@ echo "stop all things launched by ansible and stop the chroot"
 
 killAllChroot() {
 echo "killing all things remaining in the chroot"
-found=0
 for rootProc in /proc/*/root; do
         LINK=$(sudo readlink $rootProc)
         if [ "x$LINK" != "x" ]; then
@@ -17,20 +16,14 @@ for rootProc in /proc/*/root; do
                         PID=$(basename $(dirname "$rootProc"))
                         echo "$PID is in the chroot and is associated to `ps -p $PID -o comm=`, killing..."
                         sudo kill -9 "$PID"
-			found=1
                 fi
         fi
 done
-
-if [[ "$found" == "1" ]]; then
-	echo "we killed some process, we recheck to be sure that nothing is remaining"
-	killAllChroot
-fi
 }
 
 unmountFileSystems() {
         sudo echo -ne "unmounting pseudo filesystems:"
-        for pseudo in dev dev/pts proc sys
+        for pseudo in sys proc dev/pts dev
         do
                 sudo umount $root_dir/$pseudo
                 echo -ne " $pseudo"
